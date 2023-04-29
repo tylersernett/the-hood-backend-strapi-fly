4bef67aff5483fd286275b61cc225865070b52d0
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-29 15:12:46 -0400

    Add order handling

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,59 @@
 'use strict';
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const { serv_config } = require("../../../../constants");
 
 /**
  * order controller
  */
 
+const baseURL = serv_config.url.BASE_URL;
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({ //access values within strapi
+    async create(ctx) {
+        //sent from Checkout.jsx
+        const { products, userName, email } = ctx.request.body;
+        try {
+            // retrieve item information -- grab from server so client cannot manipulate
+            const lineItems = await Promise.all(
+                products.map(async (product) => {
+                    const item = await strapi
+                        .service("api::item.item")
+                        .findOne(product.id);
+
+                    return {
+                        price_data: {
+                            currency: "usd",
+                            product_data: {
+                                name: item.name,
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        quantity: product.count,
+                    };
+                })
+            );
+
+            // create a stripe session
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ["card"],
+                customer_email: email,
+                mode: "payment",
+                success_url: `${baseURL}/checkout/success`,
+                cancel_url: `${baseURL}`,
+                line_items: lineItems,
+            });
+
+            // create the item
+            await strapi
+                .service("api::order.order")
+                .create({ data: { userName, products, stripeSessionId: session.id } });
+
+            // return the session id
+            return { id: session.id };
+        } catch (error) {
+            ctx.response.status = 500;
+            return { error: { message: "There was a problem creating the charge" } };
+        }
+    }
+}));
